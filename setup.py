fd5cf0cd8b57a8c990f074db03259f07a176f004
Waleed <81301826+Wa-lead@users.noreply.github.com>
2023-07-13 06:02:31 +0300

    Changed author, description, and packages in setup.py.

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@ from setuptools import setup
 setup(
     name='Consultant',
     version='1.0',
-    author='Your Name',
-    author_email='your_email@example.com',
-    description='A package for consultants',
-    packages=['consultant'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
+    author='Waleed Alasad',
+    description='Convenient decorator for LLM code refactoring',
+    packages=['Consultant'],
+    
 )
