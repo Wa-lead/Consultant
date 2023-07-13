0cca8fe8df4ec2fc501c29a75088eee8afd37b87
Waleed <81301826+Wa-lead@users.noreply.github.com>
2023-07-13 05:52:17 +0300

    aider: Added a setup.py file for the package Consultant.
    # Context:
    USER: create a simpole setup for my package Consultant

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Consultant',
+    version='1.0',
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A package for consultants',
+    packages=['consultant'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
